a507b6290246d3bbd4a9a65e40913267842c455d
Albertoroldanq <albertoroldanq@gmail.com>
2021-09-26 12:52:47 +0100

    finishing stylinmg for projects

--- app/js/index.js
+++ app/js/index.js
@@ -1,25 +1,34 @@
-$(".menu-btn").click(function () {
-    $(".menu-btn").toggleClass("active");
-    $(".menu-icon").toggleClass("fa-times");
-    $(".menu-icon").toggleClass("fa-bars");
-    $(".overlay").toggleClass("active");
-    $(".menu-items-wrapper-mobile").toggleClass("active");
+$('.menu-btn').click(function () {
+    $('.menu-btn').toggleClass('active')
+    $('.menu-icon').toggleClass('fa-times')
+    $('.menu-icon').toggleClass('fa-bars')
+    $('.overlay').toggleClass('active')
+    $('.menu-items-wrapper-mobile').toggleClass('active')
 });
 
-$(".menu-item-mobile").click(function () {
-    $(".menu-icon").toggleClass("fa-times");
-    $(".menu-icon").toggleClass("fa-bars");
-    $(".menu-btn").removeClass("active");
-    $(".overlay").removeClass("active");
-    $(".menu-items-wrapper-mobile").toggleClass("active");
-});
+$('.menu-item-mobile').click(function () {
+    $('.menu-icon').toggleClass('fa-times')
+    $('.menu-icon').toggleClass('fa-bars')
+    $('.menu-btn').removeClass('active')
+    $('.overlay').removeClass('active')
+    $('.menu-items-wrapper-mobile').toggleClass('active')
+})
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()}
 
 function scrollFunction() {
     if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
-        document.querySelector("header").classList.remove("hidden");
+        document.querySelector('header').classList.remove('hidden')
     } else {
-        document.querySelector("header").classList.add("hidden");
+        document.querySelector('header').classList.add('hidden')
     }
 }
+
+$('.project-link').mouseenter((e) => {
+    $(e.target).addClass('no-background')
+    $(e.currentTarget).children('.project-screenshot-overlay').addClass('no-background')
+})
+$('.project-link').mouseleave((e) => {
+    $(e.target).removeClass('no-background')
+    $(e.currentTarget).children('.project-screenshot-overlay').removeClass('no-background')
+})
